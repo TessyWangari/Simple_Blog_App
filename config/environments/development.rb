3ee0e4be2f9ea21a49992f69b6098e6fce38ac20
TessyWangari <muiruritess@gmail.com>
2020-07-29 18:21:52 +0000

    Finish user signup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
    # Allow connections to local server.
   config.hosts.clear
-  config.web_console.whitelisted_ips = '41.80.243.4'
+  config.web_console.whitelisted_ips = '41.80.243.4','102.222.222.247'
 
 
 end
